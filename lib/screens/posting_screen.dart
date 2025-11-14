68844221f3abe406b0669e4c63af029121c9afec
clunckyboy <edwinjonatanpurba@gmail.com>
2025-11-14 17:29:00 +0700

    Membuat Posting Screen

--- lib/screens/posting_screen.dart
+++ lib/screens/posting_screen.dart
@@ -34,8 +34,73 @@ class PostingScreen extends StatelessWidget {
         child: Column(
           children: [
 
-          ],
+            // Halaman input
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+              child: Column(
+                children: [
+                  // Informasi
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 25,
+                        backgroundImage: AssetImage('assets/images/askalot.png'),
+                        onBackgroundImageError: null,
+                      ),
+                      // Image.asset(
+                      //   'assets/images/askalot.png',
+                      //   width: 50,
+                      // ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 18),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Blue Electric', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),),
+                            Text('11 Sep', style: TextStyle(fontSize: 18),)
+                          ],
+                        )
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  // TextField untuk input pertanyaan
+                  TextField(
+                    style: const TextStyle(color: Colors.white, fontSize: 18),
+                    maxLines: null, // Izinkan teks multi-baris
+                    decoration: InputDecoration(
+                      hintText: 'Ask something interesting!',
+                      hintStyle: TextStyle(color: Colors.grey, fontSize: 18),
+                      // Hilangkan garis bawah
+                      border: InputBorder.none,
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF7A6BFF),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text(
+                      'Add tags',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+                crossAxisAlignment: CrossAxisAlignment.start,
+              ),
+            ),
 
+            ListTile(
+              leading: Icon(Icons.gif_outlined, size: 40,),
+              title: Text('GIF'),
+            ),
+            ListTile(
+              leading: Icon(Icons.photo, size: 40,),
+              title: Text('Photo'),
+            ),
+          ],
         ),
       ),
     );
